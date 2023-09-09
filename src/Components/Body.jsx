ec80178be4487c020264f5f6049f51d69be9d1de
Daniel Yang <danielyang@Daniels-MacBook-Pro-5.local>
2023-09-09 15:07:08 -0500

    tutorial

---
+++ src/Components/Body.jsx
@@ -0,0 +1,13 @@
+import { react } from "react";
+import "./styling/Body.css"
+
+const Body = () => {
+  return (
+    <div className="bodyContainer">
+      <h1>About Me</h1>
+      <p>I am currently a student at stanford studying computer science</p>
+    </div>
+  );
+};
+
+export default Body;
